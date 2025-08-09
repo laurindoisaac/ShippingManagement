6ad1060d26c211b095f91f77082b222b255115f3
Lauri <116688165+laurindoisaac@users.noreply.github.com>
2025-08-10 04:28:39 +0700

    Add main.go
    Co-authored-by: Hajigur <66867581+hajigur69@users.noreply.github.com>

---
+++ cmd/shippingmanagement/main.go
@@ -0,0 +1,23 @@
+// cmd/shippingmanagement/main.go
+package main
+
+import (
+    "flag"
+    "log"
+    "os"
+
+    "shippingmanagement/internal/shippingmanagement"
+)
+
+func main() {
+    verbose := flag.Bool("verbose", false, "Enable verbose logging")
+    input := flag.String("input", "", "Input file path")
+    output := flag.String("output", "", "Output file path")
+    flag.Parse()
+
+    app := shippingmanagement.NewApp(*verbose)
+    if err := app.Run(*input, *output); err != nil {
+        log.Printf("Error: %v", err)
+        os.Exit(1)
+    }
+}
